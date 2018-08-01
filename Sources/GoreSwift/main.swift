da88c2df59420f826ea7108d0dcb8af372805de2
Langxxx <316230604@qq.com>
2018-08-01 12:38:48 +0800

    Parse input argument

--- Sources/GoreSwift/main.swift
+++ Sources/GoreSwift/main.swift
@@ -1,5 +1,17 @@
 import Commander
+import Foundation
 
-
-
+command (
+    Argument<String>("input", description: "The xcdatamodeld file path"),
+    Argument<String>("output", description: "Output directory for the 'XXXX+CoreDataProperties.swift' file.")
+) { input, output in
+    let inputURL = URL(fileURLWithPath: input, isDirectory: true)
+    guard let xcdatamodeldName = inputURL.lastPathComponent.components(separatedBy: ".").first else {
+        throw ArgumentError.unusedArgument("input")
+    }
+    let xmlPath = inputURL.appendingPathComponent("\(xcdatamodeldName).xcdatamodel/contents")
+    guard FileManager.default.fileExists(atPath: xmlPath.path) else {
+        throw ArgumentError.unusedArgument("input")
+    }
+}.run()
 
